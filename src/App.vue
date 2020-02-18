24bb6ae47159092fd1df22d54e1eea4105b8f9dd
Lucas Gomes <lucasgomesp22@gmail.com>
2020-02-18 18:46:07 -0300

    adding navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
+    <navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal" @toggle="toggleSliderStatus" @delete="deleteItem"></navbar>
     <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
     <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ProductList from "./components/ProductList.vue";
 import PriceSlider from "./components/PriceSlider.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
+  components: {
+    // FontAwesomeIcon,
+    ProductList,
+    PriceSlider,
+    Navbar
+  },
   data: function() {
     return {
       maximum: 99,
@@ -20,7 +29,26 @@ export default {
       products: null
     };
   },
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    }
+  },
   methods: {
+    toggleSliderStatus: function() {
+      this.sliderStatus = !this.sliderStatus;
+    },
     addItem: function(product) {
       var whichProduct;
       var existing = this.cart.filter(function(item, index) {
@@ -45,10 +73,6 @@ export default {
         this.cart.splice(id, 1);
       }
     }
-  },
-  components: {
-    ProductList,
-    PriceSlider
   },
   mounted: function() {
     fetch("https://hplussport.com/api/products/order/price")
