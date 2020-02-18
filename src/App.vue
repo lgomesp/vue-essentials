08542a1b8a06e878570ce775a5b35a38ac0f18b2
Lucas Gomes <lucasgomesp22@gmail.com>
2020-02-18 18:22:14 -0300

    adding price slider component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
-    <product-list :maximum="maximum" :products="products"></product-list>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
 import ProductList from "./components/ProductList.vue";
+import PriceSlider from "./components/PriceSlider.vue";
 
 export default {
   name: "App",
   data: function() {
     return {
       maximum: 99,
+      sliderStatus: true,
+      cart: [],
       products: null
     };
   },
+  methods: {
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+    deleteItem: function(id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    }
+  },
   components: {
-    ProductList
+    ProductList,
+    PriceSlider
   },
   mounted: function() {
     fetch("https://hplussport.com/api/products/order/price")
---
+++ src/components/PriceSlider.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="fade">
+    <div v-if="sliderStatus">
+      <div class="align-items-center" :class="sliderState">
+        <label :class="labelArr" for="formMax">max</label>
+        <input
+          type="text"
+          id="formMax"
+          class="form-control mx-2 text-center"
+          style="width: 60px;"
+          v-model="maxAmount"
+          @change="$emit('update:maximum', maxAmount)"
+        />
+        <input
+          type="range"
+          class="custom-range"
+          min="0"
+          max="200"
+          v-model="maxAmount"
+          @input="$emit('update:maximum', maxAmount)"
+        />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "price-slider",
+  data: function() {
+    return {
+      maxAmount: 99
+    };
+  },
+  props: ["sliderStatus"],
+  computed: {
+    sliderState: function() {
+      return this.sliderStatus ? "d-flex" : "d-none";
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
